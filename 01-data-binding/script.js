68fb8d8d284adfe6d70cb88d71b44334045cf12e
Alex Cabrera <alex.cabrera@gmail.com>
2021-09-15 20:26:54 +0000

    feat: data binding and dom

--- 01-data-binding/script.js
+++ 01-data-binding/script.js
@@ -1,6 +1,34 @@
 import * as d3 from "d3";
 
-let table = await d3.csv("../beer.csv");
+// Problem 1
+let textDiv = document.querySelector("#text-div");
+console.log("div");
+
+d3.select(textDiv).append("h1").text("Hello World!");
+
+// Problem 2
+let table = await d3.csv("../beer.csv", d3.autoType);
+let firstFive = table.sort((a, b) => a.brewery_id - b.brewery_id).slice(0, 5);
 console.log("script", table);
 
-let div = document.querySelector("#app");
+let dataDiv = document.querySelector("#data-div");
+
+d3.select(dataDiv)
+  .selectAll("p")
+  .data(firstFive)
+  .enter()
+  .append("p")
+  .text((d) => d.name);
+
+// Problem 3
+let barDiv = document.querySelector("#bar-div");
+
+d3.select(barDiv)
+  .append("svg")
+  .selectAll("brewery")
+  .data(firstFive)
+  .enter()
+  .append("rect")
+  .attr("width", 10)
+  .attr("height", (d) => d.ibu)
+  .attr("x", (d, i) => i * 11);
